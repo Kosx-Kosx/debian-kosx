f5b171ed2eaa273ea7596937bc695422d57b9bfe
Geoffrey Thomas <geofft@ldpreload.com>
2013-01-05 18:25:40 -0800

    fakefiles: Add support for faking dangling absolute symlinks

--- fakefiles.c
+++ fakefiles.c
@@ -1,28 +1,55 @@
+#include <errno.h>
 #include <fcntl.h>
+#include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 int
 my_open(const char *path, int oflag, mode_t mode)
 {
 	char *root, *newpath;
 	int result;
-	if (path[0] != '/') {
+
+	if (!(root = getenv("FAKEFILES_ROOT"))) {
 		goto out;
 	}
-	if (!(root = getenv("FAKEFILES_ROOT"))) {
+
+	if (path[0] != '/') {
 		goto out;
 	}
 	if (asprintf(&newpath, "%s%s", root, path) == -1) {
 		goto out;
 	}
-	if ((result = open(newpath, oflag, mode)) == -1) {
-		result = open(path, oflag, mode);
-	}
+
+	result = open(newpath, oflag, mode);
 	free(newpath);
-	return result;
+	if (result != -1) {
+		return result;
+	}
+
 out:
-	return open(path, oflag, mode);
+	result = open(path, oflag, mode);
+
+	if (result == -1 && root && errno == ENOENT) {
+		char target[PATH_MAX];
+		int n;
+		if ((n = readlink(path, target, PATH_MAX - 1)) == -1) {
+			goto out2;
+		}
+		target[n] = '\0';
+		if (target[0] != '/') {
+			goto out2;
+		}
+		if (asprintf(&newpath, "%s%s", root, target) == -1) {
+			goto out2;
+		}
+		result = open(newpath, oflag, mode);
+		free(newpath);
+	}
+
+out2:
+	return result;
 }
 
 struct {void *a; void *b;}
